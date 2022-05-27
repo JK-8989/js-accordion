6bac8e5220115f52fe7f668d934fd86baf39360c
JK-8989 <jiyoungkim365@gmail.com>
2022-05-26 21:11:55 -0700

    Add variables to js file

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hi')
+let btn = document.getElementById('hamburger-bar');
+let panelContainer = document.getElementsByClassName('panel-container');
+let questions = document.getElementsByClassName('panel-heading');
+let answers = document.getElementsByClassName('panel-body');
+
+
+
+console.log(answers)
