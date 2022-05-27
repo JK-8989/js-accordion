e62c4a8857146e21d6e3c81c1294396d485a7aec
JK-8989 <jiyoungkim365@gmail.com>
2022-05-27 00:20:47 -0700

    Edit js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let btn = document.getElementById('hamburger-bar');
 let panelContainer = document.getElementsByClassName('panel-container');
-let questions = document.getElementsByClassName('panel-heading');
+let questions = document.getElementsByClassName('panel-question');
 let answers = document.getElementsByClassName('panel-body');
 
+for (let i = 0; i < panelContainer.length; i++) {
+  panelContainer[i].addEventListener('click', () => {
 
+    for (let j = 0; j < questions.length; j++) {
+      questions[j].classList.remove('active');
+    }
+  });
+};
 
-console.log(answers)
+console.log(questions)
