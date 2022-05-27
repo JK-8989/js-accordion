4cb232e25bb10347832b8e10768645268c62edba
JK-8989 <jiyoungkim365@gmail.com>
2022-05-27 10:46:49 -0700

    Add hamburger btn function

--- script.js
+++ script.js
@@ -3,13 +3,28 @@ let panelContainer = document.getElementsByClassName('panel-container');
 let questions = document.getElementsByClassName('panel-question');
 let answers = document.getElementsByClassName('panel-body');
 
-for (let i = 0; i < panelContainer.length; i++) {
-  panelContainer[i].addEventListener('click', () => {
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener('click', (e) => {
 
-    for (let j = 0; j < questions.length; j++) {
-      questions[j].classList.remove('active');
-    }
+    for (let j = 0; j < panelContainer.length; j++) {
+      panelContainer[j].classList.remove('active');
+      e.target.parentNode.classList.add('active');
+      activateBody();
+    } 
   });
 };
 
-console.log(questions)
+activateBody = () => {
+  for (let k  = 0; k < answers.length; k++){
+    answers[k].style.display = 'none';
+  };
+  let activePanel = document.querySelector('.panel-container.active .panel-body');
+  activePanel.style.display = 'block';
+};
+activateBody()
+
+btn.addEventListener('click', ()=>{
+  for (let i  = 0; i < answers.length; i++){
+    answers[i].style.display = 'none';
+  };
+});
